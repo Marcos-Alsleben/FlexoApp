5d611b1569b23d0d16a3094d8b19026499ee8e8f
mrs_a <86418643+Marcos-Alsleben@users.noreply.github.com>
2024-11-06 07:34:24 -0300

    Desenvolvimento 30

--- FlexoApp/src/jdbc/ConnectionFactory.java
+++ FlexoApp/src/jdbc/ConnectionFactory.java
@@ -16,7 +16,7 @@ public class ConnectionFactory {
             //return DriverManager.getConnection("jdbc:mysql://localhost:3306/flexo?useSSL=false", "carton", "2574");
             //return DriverManager.getConnection("jdbc:mysql://localhost:3306/flexo", "carton", "2574");
         } catch (Exception erro) {
-            JOptionPane.showMessageDialog(null, "Erro ao conectar ao banco de dados: " + erro.getMessage());
+            JOptionPane.showMessageDialog(null, "Erro ao conectar com banco de dados: " + erro.getMessage());
             System.exit(1); // Encerra o programa
            return null; // Este retorno nunca será alcançado, mas é necessário para compilar sem erros.
         }
